bc71af9dcb7ad8ad767797777cfe4d69b5851b03
Thijs Walcarius <thijs.walcarius@ugent.be>
2019-03-05 11:53:36 +0100

    Remove NB_GID build argument

--- make-with-nvidia-docker.sh
+++ make-with-nvidia-docker.sh
@@ -4,7 +4,7 @@ BASE_DOCKER_TAG="gpulab.ilabt.imec.be:5000/jupyter"
 DOCKER_TAG_VERSION=${1:-""}
 BASE_CONTAINER=${2:-"nvcr.io/nvidia/cuda:10.0-cudnn7-runtime-ubuntu18.04"}
 
-DARGS="--build-arg NB_GID=7176 --build-arg BASE_CONTAINER=\"$BASE_CONTAINER\"" make build/base-notebook
+DARGS="--build-arg BASE_CONTAINER=\"$BASE_CONTAINER\"" make build/base-notebook
 
 declare -a docker_images=("minimal-notebook" \
 	"r-notebook" \
@@ -21,6 +21,7 @@ do
 	if [[ -n "$DOCKER_TAG_VERSION" ]]; then
 		docker tag "$BASE_DOCKER_TAG/$docker_image:latest" "$BASE_DOCKER_TAG/$docker_image:$DOCKER_TAG_VERSION"
 		docker push "$BASE_DOCKER_TAG/$docker_image:$DOCKER_TAG_VERSION"		
+		docker push "$BASE_DOCKER_TAG/$docker_image:latest"		
 	fi
 done
 
